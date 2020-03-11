2934816586496d3a4f58752d558d2cdc436d0008
‘amornpan’ <amornpan@gmail.com>
2020-03-11 13:05:42 +0700

    develop-three_features-function

--- function.php
+++ function.php
@@ -1,3 +1,6 @@
+function add(a,b) {
+    return a+b;
+}
 function convert-c-to-k(c){return c+273;}
 function multiple-of-3(n){return n*3;}
 function division-of-2-number(a,b){if (b!=0) return a/b; return 0;}
